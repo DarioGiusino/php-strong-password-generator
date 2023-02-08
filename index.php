0a7ad571e6f620ae286c4c7f168bfa094e464db1
DarioGiusino <dario.giusino@gmail.com>
2023-02-08 15:26:02 +0100

    genetared a random string

--- index.php
+++ index.php
@@ -1,6 +1,55 @@
 <?php
+$length = (int)isset($_GET['psw-length']) ?? null;
 
+$permit = array();
 
+function random_psw($length, $args = array())
+{
+
+    $length = (int)$_GET['psw-length'];
+
+    $permit = array(); //contiene i gruppi di caratteri permessi
+
+    //se non è stato passato alcun argomento, rendo tutti i gruppi disponibili
+    //altrimenti rendo disponibili solo i gruppi abilitati in $args
+    if (empty($args)) {
+        $permit[] = 'alfa_upper';
+        $permit[] = 'alfa_lower';
+        $permit[] = 'number';
+        $permit[] = 'simbol';
+    } else {
+        $permit = $args;
+    }
+
+    $characters = '';
+
+    if (in_array('alfa_upper', $permit)) {
+        $characters .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+
+    if (in_array('alfa_lower', $permit)) {
+        $characters .= 'abcdefghijklmnopqrstuvwxyz';
+    }
+
+    if (in_array('number', $permit)) {
+        $characters .= '1234567890';
+    }
+
+    if (in_array('simbol', $permit)) {
+        $characters .= '!"£$%&/()=?\'^@#[]*';
+    }
+
+    // controllo di sicurezza: se è stato passato un valore errato a $length, prendo 32 come default
+    if (!is_numeric($length)) {
+        $length = 32;
+    }
+
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $password .= substr($characters, rand(0, strlen($characters) - 1), 1);
+    }
+    return $password;
+}
 
 ?>
 
@@ -28,14 +77,14 @@
     <main class="container">
         <!-- alert -->
         <div class="alert alert-primary my-4" role="alert">
-            A simple primary alert—check it out!
+            <?= $length ? random_psw($length, $permit) : 'Inserisci la lunghezza desiderata' ?>
         </div>
 
         <!-- form -->
         <form action="#" method="GET">
             <div class="mb-3 w-50">
                 <label for="psw-length" class="form-label">Lunghezza password:</label>
-                <input type="number" class="form-control" id="psw-length" min="1" step="1">
+                <input type="number" class="form-control" id="psw-length" min="1" max="32" step="1" name="psw-length">
             </div>
             <button class="btn btn-primary">Invia</button>
         </form>
